5f7dcbacefba90cd0d3a037dc6949748957c344c
IndraHardika <92752408+KZdra@users.noreply.github.com>
2023-12-28 23:57:12 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+onload = () =>{
+        document.body.classList.remove("container");
+        alert ("BIG LUV LUV <3 !");
+        var audio = document.getElementById('background-audio');
+        audio.play();
+};
+
